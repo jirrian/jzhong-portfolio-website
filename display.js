f8d8f23e8f23635f32482fdcc5bf034d58703467
Jillian <jillianzhong@yahoo.com>
2019-09-03 02:23:12 -0400

    implemented displaying details

--- display.js
+++ display.js
@@ -18,42 +18,36 @@ function setUp(){
 	request.onload = function() {
   		works = request.response;
   		//console.log(works);
+  		showAll();
   	}
+  	
+}
+
+function showAll(){
+	toggleButtons(document.getElementById("all"));
+
+	//display all works
+	for(var i = 0; i < works['works'].length; i++){
+		makePreviewDivs(works['works'][i]);
+	}
 }
 
 function showMedium(){
-	//turn this button red
-	this.parentNode.style.backgroundColor = "red";
-	//this.style.color = "white";
-	//this.classList.add("medium-selected");
+	toggleButtons(this);
 
-	//turn all others back to default white
-	var mediums = document.getElementsByClassName("medium");
-	for(var i = 0; i < mediums.length; i++){
-		if(mediums[i].id != this.id){
-			mediums[i].parentNode.style.backgroundColor = "white";
-			//mediums[i].style.color = "black";
-			//this.classList.remove("medium-selected");
-		}
-	}
-	//console.log(works);
-	//clear all divs
-	var wrapper = document.getElementById("wrapper");
-  	while (wrapper.firstChild) {
-    	wrapper.removeChild(wrapper.firstChild);
-  	}
+	clearDivs();
 
 	//display relevant works based on medium
 	for(var i = 0; i < works['works'].length; i++){
 		if (works['works'][i]['mediums'].includes(this.id)){
 			console.log(works['works'][i]);
-			makeDivs(works['works'][i]);
+			makePreviewDivs(works['works'][i]);
 		}
 	}
 
 }
 
-function makeDivs(work){
+function makePreviewDivs(work){
 	// get wrapper div
 	var wrapper = document.getElementById("wrapper");
 
@@ -62,9 +56,15 @@ function makeDivs(work){
 	work_div.classList.add("content");
 
 	// create first image
-	var work_image = document.createElement("img");
-	work_image.src = 'portfolio_images/' + work['photos'][0];
-	work_div.appendChild(work_image);
+	if(work['photos'] != null){
+		var work_image = document.createElement("img");
+		work_image.src = 'portfolio_images/' + work['photos'][0];
+		work_div.appendChild(work_image);
+	}
+	else{
+		//embed video
+	}
+
 
 	// create title text
 	var work_title = document.createElement("span");
@@ -76,8 +76,107 @@ function makeDivs(work){
 	}
 	work_title.classList.add("name");
 	work_div.appendChild(work_title); 
+
+	// div to hold details
+	var work_detail_div = document.createElement("div");
+
+	// create description
+	var work_description = document.createElement("p");
+	work_description.innerText = work["description"]["medium"] + "\n" + work["description"]["concept"];
+	work_detail_div.appendChild(work_description);
+
+	// create rest of photos
+	if(work['photos'].length > 0){
+		for(var i = 1; i < work['photos'].length; i++){
+			var image = document.createElement("img");
+			image.src = 'portfolio_images/' + work['photos'][i];
+			work_detail_div.appendChild(image);
+		}
+	}
+
+	// create links
+	if(work['links']['video'] != null){
+		//embed video
+	}
+
+	if(work['links']['blog'] != null){
+		var blog_heading = document.createElement("p");
+		blog_heading.innerText = "Documentation: \n"
+		work_detail_div.appendChild(blog_heading);
+
+		//list blog links
+		for(var i = 0; i < work['links']['blog'].length; i++){
+			var link = document.createElement("a");
+			link.setAttribute('href', work['links']['blog'][i]);
+			link.setAttribute('target', "_blank");
+
+			fetch(work['links']['blog'][i]).then((response) => response.text()).then((html) => {
+		      const doc = new DOMParser().parseFromString(html, "text/html");
+		      const title = doc.querySelectorAll('title')[0];
+		      link.innerText =  title.innerText;
+    		});
+
+    		blog_heading.appendChild(link);
+		}
+	}
+
+	if(work['links']['demo'] != null){
+		var demo_link = document.createElement("a");
+		demo_link.setAttribute('href', work['links']['demo']);
+		demo_link.setAttribute('target', "_blank");
+		demo_link.innerText = "View Live";
+		demo_link.classList.add("demo_link");
+
+		work_detail_div.appendChild(demo_link);
+	}
+
+	//make detail hidden by default
+	work_detail_div.style.display = "none";
+
+	// add eventlistner to show work details if div is clicked 
+	work_div.addEventListener("click", function(){
+		showDetailDiv(work_detail_div);
+	}, false);
+
+	work_div.appendChild(work_detail_div);
+
 	wrapper.appendChild(work_div);
 
+}
+
+// toggle details
+function showDetailDiv(work_detail_div){
+	if(work_detail_div.style.display == "block"){
+		work_detail_div.style.display = "none";
+	}
+	else{
+		work_detail_div.style.display = "block";
+	}
+}
+
+function clearDivs(){
+	//clear all divs
+	var wrapper = document.getElementById("wrapper");
+  	while (wrapper.firstChild) {
+    	wrapper.removeChild(wrapper.firstChild);
+  	}
+}
+
+function toggleButtons(medium_linkelem){
+		//turn this button red
+	medium_linkelem.parentNode.style.backgroundColor = "red";
+	//this.style.color = "white";
+	//this.classList.add("medium-selected");
+	medium_linkelem.parentNode.classList.add("navi-medium-selected");
+
+	//turn all others back to default white
+	var mediums = document.getElementsByClassName("medium");
+	for(var i = 0; i < mediums.length; i++){
+		if(mediums[i].id != medium_linkelem.id){
+			mediums[i].parentNode.style.backgroundColor = "white";
+			mediums[i].parentNode.classList.remove("navi-medium-selected");
+		}
+	}
 }
 
 function showAbout(){
@@ -97,22 +196,4 @@ function showPress(){
 	document.getElementById("press").style.display = "block";
 	document.getElementById("about").style.display = "none";
 	document.getElementById("contact").style.display = "none";
-}
-
-function showWorks(button, medium){
-	
-
-	if (medium == "interactive"){
-	}
-	else if(medium == "media"){
-	}
-	else if(medium == "appareldesign"){
-	}
-	else if(medium == "web"){
-	}
-	else if(medium == "installation"){
-	}
-	else if(medium == "creativedirection"){
-
-	}
 }
