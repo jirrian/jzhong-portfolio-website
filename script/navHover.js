468f2064741af1358ee3a3df505000cf241dba23
jirrian <jillianzhong@yahoo.com>
2021-02-22 17:45:15 -0500

    updated hover nav script, fixed responsive sizing of cards/images

--- script/navHover.js
+++ script/navHover.js
@@ -3,53 +3,55 @@ var mediums = textWrapper.querySelectorAll('.nav-link-hide');
 
 var animationEnter;
 var animationLeave;
-document.getElementById('navBar').addEventListener("mouseenter", (event) => {
-    if( window.innerWidth >= 1200 ){
-            if (animationLeave){
-        animationLeave.remove(textWrapper.querySelectorAll('.ml12 .letter'));
-        }
-        anime.set(mediums, {display : "block"});
-        anime.set(textWrapper.querySelectorAll('.ml12 .letter'),{ translateX: 0});
-        animationEnter = anime.timeline({loop: false})
-        .add({
-            targets: '.nav-link-hide',
-            translateX: [5,0],
-            opacity: [0,1],
-            translateZ: 0,
-            easing: "easeOutExpo",
-            duration: 1200,
-            delay: (el, i) => 100 + 30 * i
-          });
-    }
-});
-
-
-document.getElementById('navBar').addEventListener("mouseleave", (event) => {
-    if( window.innerWidth >= 1200 ){
-        console.log(window.screen.availWidth);
-        if (animationEnter){
-            animationEnter.remove(textWrapper.querySelectorAll('.ml12 .letter'));
-        }
-        animationLeave = anime.timeline({loop: false})
+if(document.getElementById('navBar')){
+    document.getElementById('navBar').addEventListener("mouseenter", (event) => {
+        if( window.innerWidth >= 1200 ){
+                if (animationLeave){
+            animationLeave.remove(textWrapper.querySelectorAll('.ml12 .letter'));
+            }
+            anime.set(mediums, {display : "block"});
+            anime.set(textWrapper.querySelectorAll('.ml12 .letter'),{ translateX: 0});
+            animationEnter = anime.timeline({loop: false})
             .add({
-                targets: '.ml12 .letter',
-                translateX: [0,-30],
-                opacity: [1,0],
-                easing: "easeInExpo",
-                duration: 1100,
+                targets: '.nav-link-hide',
+                translateX: [5,0],
+                opacity: [0,1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 1200,
                 delay: (el, i) => 100 + 30 * i
-            })
-        animationLeave.finished.then(function(){
-            anime.set(mediums, {display : "none"});
-            anime.timeline({loop: false}).add({
-                    targets: '.nav-link-show',
-                    translateX: [5,0],
-                    opacity: [0,1],
-                    translateZ: 0,
-                    easing: "easeOutExpo",
-                    duration: 1200,
+              });
+        }
+    });
+
+
+    document.getElementById('navBar').addEventListener("mouseleave", (event) => {
+        if( window.innerWidth >= 1200 ){
+            console.log(window.screen.availWidth);
+            if (animationEnter){
+                animationEnter.remove(textWrapper.querySelectorAll('.ml12 .letter'));
+            }
+            animationLeave = anime.timeline({loop: false})
+                .add({
+                    targets: '.ml12 .letter',
+                    translateX: [0,-30],
+                    opacity: [1,0],
+                    easing: "easeInExpo",
+                    duration: 1100,
                     delay: (el, i) => 100 + 30 * i
+                })
+            animationLeave.finished.then(function(){
+                anime.set(mediums, {display : "none"});
+                anime.timeline({loop: false}).add({
+                        targets: '.nav-link-show',
+                        translateX: [5,0],
+                        opacity: [0,1],
+                        translateZ: 0,
+                        easing: "easeOutExpo",
+                        duration: 1200,
+                        delay: (el, i) => 100 + 30 * i
+                });
             });
-        });
-    }
-});
+        }
+    });
+}
