cf9408a73018194fafc46c7d1be961bce9fb4e08
jirrian <jillianzhong@yahoo.com>
2021-02-26 00:30:47 -0500

    generated new placeholders, added new assets, fixed code for resizing local videos

--- script/imageResize.js
+++ script/imageResize.js
@@ -1,27 +1,33 @@
-window.addEventListener('load', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
 	if( window.innerWidth >= 1200 ){
 		var content = document.querySelectorAll('.work-images .col-');
 		//or however you get a handle to the IMG
 		content.forEach(function(col){
 			var img = col.querySelector('img');
 			if(img){
-				var ratio = img.naturalHeight / img.naturalWidth;
-				if(ratio > 1){
-					col.classList.add("col-6");
-				}
+				img.addEventListener('load', (event) => {
+					var ratio = img.naturalHeight / img.naturalWidth;
+					if(ratio >= 1){
+						col.classList.add("col-6");
+					}
+				});
 			}
 			var vid = col.querySelector('video');
 			if(vid){
-				var ratio = vid.videoHeight / vid.videoWidth;
-				if(ratio > 1){
-					col.classList.add("col-6");
-				}
+				vid.addEventListener('loadeddata', (event) => {
+					if(vid.readyState >= 1) {
+						var ratio = vid.videoHeight / vid.videoWidth;
+						if(ratio >= 1){
+							col.classList.add("col-6");
+						}
+					}
+				});
 			}
 			var div = col.querySelector('.ratio');
 			if(div){
 				var style = window.getComputedStyle(div);
 				var ratio = style.getPropertyValue('--bs-aspect-ratio');
-				if(parseFloat(ratio) / 100.0 > 1){
+				if(parseFloat(ratio) / 100.0 >= 1){
 					col.classList.add("col-6");
 				}
 			}
